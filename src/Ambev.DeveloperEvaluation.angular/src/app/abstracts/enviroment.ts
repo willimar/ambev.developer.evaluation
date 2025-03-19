a8afd6b7ce54e164dc41280e447f087944760b7f
Willimar Rocha <willimar@gmail.com>
2025-03-19 15:52:09 -0300

    removendo o token fixo

--- src/Ambev.DeveloperEvaluation.angular/src/app/abstracts/enviroment.ts
+++ src/Ambev.DeveloperEvaluation.angular/src/app/abstracts/enviroment.ts
@@ -1,7 +1,7 @@
 export const Enviroment = {
     production: false,
     api: 'https://localhost:7181/api/',
-    token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhMWY2YmI3OS0wM2JhLTRiOWEtOWQxYi1hYWM2ODdlNjVhNjIiLCJ1bmlxdWVfbmFtZSI6IndpbGxpbWFyIiwicm9sZSI6Ik1hbmFnZXIiLCJuYmYiOjE3NDI0MDAxOTYsImV4cCI6MTc0MjQyODk5NiwiaWF0IjoxNzQyNDAwMTk2fQ.UdyH4ir3DEX5CedNEOptFvEJbrLRB-EkUOM4lqEddmE`,
+    token: ``,
     isAuthenticaded: () => {
         return Enviroment.token != ``;
     },
